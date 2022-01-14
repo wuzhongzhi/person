30e258eea4c26f1999f82ef178b0bd9715dd7390
wuzhongzhi <38462987+wuzhongzhi@users.noreply.github.com>
2022-01-14 14:44:34 +0800

    Delete code directory

--- code/Send.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.lecode.proxyDemo;
-
-public class Send {
-    public void SendMsg(String msg){
-        System.out.println("msg is "+msg);
-    }
-}
--- code/SendMsgService.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.lecode.proxyDemo;
-
-public interface SendMsgService {
-    void send(String msg);
-    void call(String call);
-}
